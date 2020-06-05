9f6a841b54116785af7ec852cfcecc47f37cd951
Zach Sakar <zach.sakar@gmail.com>
2020-06-05 13:55:23 -0600

    Fixed api connection bug

--- api_connect.js
+++ api_connect.js
@@ -1,19 +1,17 @@
-async function queryApis(platformInput, nameInput) {
+function queryApis(platformInput, nameInput) {
 
 
-
+    console.log('test a')
     const postsPromise = fetch('https://apex-legends-tracker-api.herokuapp.com/', {
         method: 'GET',
-        mode: 'no-cors',
         headers: {
             platform: platformInput,
             name: nameInput,
         }
-    });
-
-    const posts = await postsPromise.then(res =>{
-        console.log(postsPromise)
-    }); 
-
-    return postsPromise;
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log("Error querying API: ", error))
 }
+
+queryApis('origin', 'tvc_hazed');
